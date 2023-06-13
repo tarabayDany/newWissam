4ea670d2eb73883425535b5a0de2b124feaba5c0
tarabayDany <dany@quiqup.com>
2023-06-13 15:50:34 +0300

    fix2

---
+++ hello.py
@@ -0,0 +1,60 @@
+import pandas as pd 
+def gender():
+    # Load the dataset
+    gender_data = pd.read_csv("gender.csv")
+
+    # Create a range slider for year selection
+    # year_range = st.slider("Select year range", 2000, 2020, (2000, 2020))
+    year_range = (2000, 2020)
+
+    # Filter the dataset based on selected years
+    filtered_data = gender_data[
+        (gender_data["Year"] >= year_range[0]) & (gender_data["Year"] <= year_range[1])
+    ]
+
+    # Remove rows with NaN values in the 'Population (historical estimates)' column
+    filtered_data = filtered_data.dropna(subset=["Population (historical estimates)"])
+
+    # Exclude the entity "World" from the filtered data
+    filtered_data = filtered_data[filtered_data["Entity"] != "World"]
+    # print(filtered_data.Entity.unique())
+
+    # Aggregate the data by taking the average smoking percentages and population for each country
+    agg_data = filtered_data.groupby(["Entity"]).count().reset_index()
+    # print(agg_data)
+    # print(agg_data.mean())
+
+    # agg_data2 = agg_data.mean().reset_index()
+
+    # # Function to filter countries by region
+    # def filter_countries(data, region):
+    #     if region == "mena":
+    #         mena_countries = [
+    #             "Bahrain",
+    #             "Cyprus",
+    #             "Egypt",
+    #             "Iran",
+    #             "Iraq",
+    #             "Israel",
+    #             "Jordan",
+    #             "Kuwait",
+    #             "Lebanon",
+    #             "Libya",
+    #             "Morocco",
+    #             "Oman",
+    #             "Palestine",
+    #             "Qatar",
+    #             "Saudi Arabia",
+    #             "Syria",
+    #             "Tunisia",
+    #             "Turkey",
+    #             "United Arab Emirates",
+    #             "Yemen",
+    #         ]
+    #         return data[data["Entity"].isin(mena_countries)]
+    #     else:
+    #         return data
+
+
+if __name__ == "__main__":
+    gender()
