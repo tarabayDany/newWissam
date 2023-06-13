9d1a2e093a7a1a5f5307ed7ceaa30f13d436b2c1
tarabayDany <dany@quiqup.com>
2023-06-13 15:41:57 +0300

    fix1

--- WM_HCA_Individual_Project.py
+++ WM_HCA_Individual_Project.py
@@ -406,7 +406,7 @@ def gender():
     filtered_data = filtered_data[filtered_data["Entity"] != "World"]
 
     # Aggregate the data by taking the average smoking percentages and population for each country
-    agg_data = filtered_data.groupby(["Entity"]).mean().reset_index()
+    agg_data = filtered_data.groupby(["Entity"]).astype(float).mean().reset_index()
 
     # Calculate the scaling factor for the size of the circles
     circle_scaling_factor = 0.00005
